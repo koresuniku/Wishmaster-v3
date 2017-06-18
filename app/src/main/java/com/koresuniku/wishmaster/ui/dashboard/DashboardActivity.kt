ea861f867d6117b0afe488f25eba98c6dffdb5db
admin <koresuniku@gmail.com>
2017-06-18 16:42:12 +0300

    Initial commit

---
+++ app/src/main/java/com/koresuniku/wishmaster/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.koresuniku.wishmaster.ui.dashboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.koresuniku.wishmaster.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+
+
+}
